fefa95fa73813982b8ca070f513f1268e0e27839
tetresbr <mmoura.siqueira@gmail.com>
2021-02-17 09:43:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,28 +13,28 @@ def calculo():
     if (inss < 0):
         inss = 0
 
-#IRPF
+#IRRF
     if (salario <= 1903.98):
-        irpf = 0
+        irrf = 0
     elif (salario <= 2826.60):
-        irpf = round((salario * 0.075) - 142.80,2)
+        irrf = round((salario * 0.075) - 142.80,2)
     elif (salario <= 3751.05):
-        irpf = round((salario * 0.15) - 354.80,2)
+        irrf = round((salario * 0.15) - 354.80,2)
     elif (salario <= 4664.68):
-        irpf = round((salario * 0.225) - 636.13,2)
+        irrf = round((salario * 0.225) - 636.13,2)
     else:
-        irpf = round((salario * 0.275) - 869.36,2)
+        irrf = round((salario * 0.275) - 869.36,2)
 
 
-    if (irpf < 0):
-        irpf = 0
+    if (irrf < 0):
+        irrf = 0
 
-    impostos = inss+irpf
+    impostos = inss+irrf
     salario_liquido = round(salario - impostos,2)
 
     print(f'O seu salario bruto é R${salario}')
     print(f'Você paga R${inss} de INSS')
-    print(f'Você paga R${irpf} de IRPF')
+    print(f'Você paga R${irrf} de IRPF')
     print(f'Seu Salario Liquido é R${salario_liquido}')
 
 def get_salario():
